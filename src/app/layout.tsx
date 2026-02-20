921fc474b5507196b7acc39cab41cb809a5b811f
jmgee <84143379+jmgee@users.noreply.github.com>
2026-02-20 13:54:20 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,16 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { cookies } from "next/headers";
-import SplashGate from "@/components/SplashGate";
+import SplashGate from "@/components/mxds/SplashGate";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://mdevelopments.xyz"),
-  title: "M Developments",
-  description: "FiveM Developer Portfolio",
 };
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const hasSeen = cookies().get("mxds_splash_seen")?.value === "1";
-  const showSplash = !hasSeen;
 
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <SplashGate
-          show={showSplash}
-          iconSrc="https://r2.fivemanage.com/j8pmvTQ4T0tTaPRfv1JNy/m1024.png"
-        >
+        <SplashGate iconSrc="https://r2.fivemanage.com/j8pmvTQ4T0tTaPRfv1JNy/m1024.png">
           {children}
         </SplashGate>
       </body>
